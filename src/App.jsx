1483bd670a9e5266ce2b90bfab81089aad4d86d0
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-07-30 17:42:14 -0400

    Completes lab with Jeffy as lab partner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
+function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [currentPoint, setCurrentPoint] = useState(1);
+
+  const changeScore = () => {
+    setCurrentScore(currentScore + currentPoint);
+  };
+
+  const changePoint = () => {
+    if (currentScore >= 10) {
+      setCurrentPoint(currentPoint + 1);
+      setCurrentScore(currentScore - 10);
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+
+  const resetGame = () => {
+    setCurrentPoint(1)
+    setCurrentScore(0)
+  }  
+
+  if(currentScore >= 100) {
+     return ( 
       <main>
-        <p>React State Lab</p>
-      </main>
-    );
+      <h1>Current Score: {currentScore}</h1>
+     <h2>You win!</h2>
+     <button onClick={resetGame}>Play again?</button>
+     </main>
+     )
+  }
+  return (
+    <main>
+      <h1>Current Score: {currentScore}</h1>
+      <h2></h2>
+      <button onClick={changeScore}>+{currentPoint}</button>
+      <button onClick={changePoint}>
+        Pay 10 points to change from +{currentPoint} to +{currentPoint + 1}
+      </button>
+    </main>
+  );
 }
 
 export default App;
